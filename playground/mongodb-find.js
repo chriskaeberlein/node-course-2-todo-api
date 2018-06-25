ef1cc88c5750e90ab921289a2d6bd2c96e3752b7
ckaeberlein <ckaeberlein@ebsco.com>
2018-06-24 20:50:21 -0400

    Initial commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,24 @@
+// const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err,db) => {
+  if (err) {
+    return console.log('Unable to connect to Mongodb server.');
+  }
+  console.log('Connected to Mongodbserver.');
+
+  db.collection('Users').find({name: 'Jen'}).toArray().then((docs) => {
+    console.log('Users');
+    console.log(JSON.stringify(docs, undefined, 2));
+  }, (err) => {
+    console.log('Unable to fetch Users', err);
+  });
+
+  db.collection('Todos').find().count().then((count) => {
+    console.log(`Todos count: ${count}`);
+  }, (err) => {
+    console.log('Unable to fetch Todos', err);
+  });
+
+  // db.close();
+});
