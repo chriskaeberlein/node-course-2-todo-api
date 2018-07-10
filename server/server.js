b9e0ab5d1717c9b104270c723491e063e105bb67
ckaeberlein <ckaeberlein@ebsco.com>
2018-07-10 19:59:22 -0400

    Added get todo by id route

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+var {ObjectID} = require('mongodb');
+
 var express = require('express');
 var bodyParser = require('body-parser');
 
@@ -27,6 +29,23 @@ app.get('/todos', (req, res) => {
   }, (e) => {
     res.status(400).send(e);
   })
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+
+  if (!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  } else {
+    Todo.findById(id).then((todo) => {
+      if (!todo) {
+       res.status(404).send();
+      }
+      else {
+       res.status(200).send({todo});
+    }
+  }).catch((e) => res.status(400).send());
+  }
 });
 
 app.listen(3000, () => {
