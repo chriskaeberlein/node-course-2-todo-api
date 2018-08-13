83da7680d368c3f019c655b5ceb06a6ee7c7f6e4
ckaeberlein <ckaeberlein@ebsco.com>
2018-08-13 19:38:05 -0400

    Hash user password before saving

--- playground/hashing.js
+++ playground/hashing.js
@@ -1,12 +1,27 @@
 const {SHA256} = require('crypto-js');
 const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs');
 
-var data = {
-  id: 10
-};
+var password = 'abc123!';
 
-var token = jwt.sign(data, 'abc123');
-console.log(token);
+// bcrypt.genSalt(10, (err, salt) => {
+//   bcrypt.hash(password, salt, (err, hash) => {
+//     console.log(hash);
+//   });
+// });
 
-var decoded = jwt.verify(token, 'abc123');
-console.log('decoded', decoded);
+var hashedPassword = '$2a$10$pAYt/o247hj/gG4KVEzf8ub8cm4qVTCGwhCST5pDLcEUT/J4jpfsW';
+
+bcrypt.compare(password, hashedPassword, (err, res) => {
+  console.log(res);
+});
+
+// var data = {
+//   id: 10
+// };
+//
+// var token = jwt.sign(data, 'abc123');
+// console.log(token);
+//
+// var decoded = jwt.verify(token, 'abc123');
+// console.log('decoded', decoded);
